7a13202727794603893159c800cf86ebdb6ac66a
RodolfoAlvarenga <rodolfo.l.alvarenga@gmail.com>
2021-01-25 17:27:24 -0300

    Finish Catalog Loaders and imports refactoring

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { ProductsResponse } from '../../core/types/Product';
-import { makeRequest } from '../../core/utils/request';
+import { ProductsResponse } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request';
 import ProductCard from './components/ProductCard';
+import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import './styles.scss';
 
 const Catalog = () => {
   const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const params = {
@@ -14,20 +16,27 @@ const Catalog = () => {
       linesPerPage: 12,
     };
 
-    makeRequest({ url: '/products', params }).then((response) =>
-      setProductsResponse(response.data)
-    );
+    setIsLoading(true);
+    makeRequest({ url: '/products', params })
+      .then((response) => setProductsResponse(response.data))
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
   return (
     <div className="catalog-container">
       <h1 className="catalog-title">Catálogo de produtos</h1>
       <div className="catalog-products">
-        {productsResponse?.content.map((product) => (
-          <Link to={`/products/${product.id}`} key={product.id}>
-            <ProductCard product={product} />
-          </Link>
-        ))}
+        {isLoading ? (
+          <ProductCardLoader />
+        ) : (
+          productsResponse?.content.map((product) => (
+            <Link to={`/products/${product.id}`} key={product.id}>
+              <ProductCard product={product} />
+            </Link>
+          ))
+        )}
       </div>
     </div>
   );
