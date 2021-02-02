c8b801fc60c466d8c883f28c66c876b136f3fa0f
RodolfoAlvarenga <rodolfo.l.alvarenga@gmail.com>
2021-02-02 17:04:18 -0300

    POST new Product - Forms

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,16 +1,95 @@
-import React from 'react';
+import { makeRequest } from 'core/utils/request';
+import React, { useState } from 'react';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
 
+type FormState = {
+  name: string;
+  price: string;
+  category: string;
+  description: string;
+};
+
+type FormEvent = React.ChangeEvent<
+  HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+>;
+
 const Form = () => {
+  const [formData, setFormData] = useState<FormState>({
+    name: '',
+    price: '',
+    category: '',
+    description: '',
+  });
+
+  const handleOnChange = (event: FormEvent) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData((data) => ({ ...data, [name]: value }));
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const payload = {
+      ...formData,
+      imgUrl:
+        'https://images7.kabum.com.br/produtos/fotos/128647/controle-microsoft-xbox-sem-fio-preto-qat-00007_1601326881_g.jpg',
+      categories: [{ id: formData.category }],
+    };
+
+    makeRequest({ url: '/products', method: 'POST', data: payload }).then(
+      () => {
+        setFormData({ name: '', category: '', price: '', description: '' });
+      }
+    );
+  };
+
   return (
-    <BaseForm title="cadastrar um produto">
-      <div className="row">
-        <div className="col-6">
-          <input type="text" className="form-control" />
+    <form onSubmit={handleSubmit}>
+      <BaseForm title="cadastrar um produto">
+        <div className="row">
+          <div className="col-6">
+            <input
+              value={formData.name}
+              name="name"
+              type="text"
+              className="form-control mb-5"
+              onChange={handleOnChange}
+              placeholder="Nome do produto"
+            />
+            <select
+              value={formData.category}
+              name="category"
+              className="form-control mb-5"
+              onChange={handleOnChange}
+            >
+              <option value="1">Livros</option>
+              <option value="3">Computadores</option>
+              <option value="2">Eletrônicos</option>
+            </select>
+            <input
+              value={formData.price}
+              name="price"
+              type="text"
+              className="form-control"
+              onChange={handleOnChange}
+              placeholder="Preço"
+            />
+          </div>
+          <div className="col-6">
+            <textarea
+              value={formData.description}
+              name="description"
+              className="form-control"
+              onChange={handleOnChange}
+              cols={30}
+              rows={10}
+            />
+          </div>
         </div>
-      </div>
-    </BaseForm>
+      </BaseForm>
+    </form>
   );
 };
 
